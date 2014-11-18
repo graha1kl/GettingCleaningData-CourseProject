586f98e94c5b6ccc7a8af49fe6f096fea45ec889
Krista Graham <kgraham13@gsu.edu>
2014-11-18 14:47:31 -0500

    initial commit of run_analysis script

---
+++ run_analysis.R
@@ -0,0 +1,74 @@
+# Coursera: Getting and Cleaning Data
+# Course Project
+
+# Download and unzip the data from this source:
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+library(plyr)
+
+# Step 1: Merge the training and test sets to create one data set
+
+# set working directory to location where the dataset was unzipped
+setwd('C:/Users/kgraham13/Desktop/Coursera/CleaningData/Project/UCI HAR Dataset/')
+
+# read in data from test and training files & assign column names to subject and
+# activity tables
+subjectTest <- read.table("test/subject_test.txt", col.names="SubjectID")
+xTest <- read.table("test/X_test.txt")
+yTest <- read.table("test/y_test.txt", col.names="ActivityID")
+
+subjectTrain <- read.table("train/subject_train.txt", col.names="SubjectID")
+xTrain <- read.table("train/X_train.txt")
+yTrain <- read.table("train/y_train.txt", col.names="ActivityID")
+
+# combine data into one table
+testData <- cbind(subjectTest, yTest, xTest)
+trainingData <- cbind(subjectTrain, yTrain, xTrain)
+mergedData <- rbind(testData, trainingData)
+
+# Step 2: Extract only the measurements on the mean and standard deviation for 
+#         each measurement. 
+
+# read in data from features table
+features <- read.table("features.txt", stringsAsFactors=FALSE)
+
+# extract features that measure mean or standard deviation
+featuresMeanStd <- features[grep("mean\\(\\)|std\\(\\)", features$V2), ]
+
+# subset mergedData to retain only columns that measure mean or standard deviation
+meanStdData <- mergedData[ ,c(1, 2, featuresMeanStd$V1+2)]
+
+# Step 3: Use descriptive activity names to name the activities in the data set
+
+# read in data from activity_labels table
+aLabels <- read.table("activity_labels.txt", stringsAsFactors=FALSE)
+
+# replace ActivityIDs with label names
+meanStdData$ActivityID <- aLabels[meanStdData$ActivityID, 2]
+
+# Step 4: Appropriately label the data set with descriptive variable names
+
+# create a vector of column/feature names
+columnNames <- c("SubjectID", "Activity", featuresMeanStd$V2)
+
+# clean feature names
+for (i in 1:length(columnNames)) {
+        columnNames[i] <- gsub("-mean\\(\\)", "_Mean", columnNames[i])
+        columnNames[i] <- gsub("-std\\(\\)", "_StdDev", columnNames[i])
+        columnNames[i] <- gsub("-X", "_X", columnNames[i])
+        columnNames[i] <- gsub("-Y", "_Y", columnNames[i])
+        columnNames[i] <- gsub("-Z", "_Z", columnNames[i])
+        columnNames[i] <- gsub("BodyBody","Body", columnNames[i])
+}
+
+# apply cleaned names as column headers
+colnames(meanStdData) <- columnNames
+
+# Step 5:  Create a second, independent tidy data set with the average of each
+#          variable for each activity and each subject
+
+# create table containing averages for each variable by subject and activity
+meanStdDataAv <- ddply(meanStdData, c("SubjectID", "Activity"), numcolwise(mean))
+
+# write tidy data to text file
+write.table(meanStdDataAv, file="tidyDataAverages.txt", row.name=FALSE)
